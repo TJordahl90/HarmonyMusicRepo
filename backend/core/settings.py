7cfe056927d824d145451ceba03d561cf54fdda3
TJordahl90 <143350615+TJordahl90@users.noreply.github.com>
2024-12-10 02:35:31 -0600

    put in static root

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = ''
@@ -133,7 +133,7 @@ EMAIL_HOST_PASSWORD = 'slvu hurc zrde fnmq'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
