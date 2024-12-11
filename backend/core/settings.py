88a4d976d1f81b96f39c40349341aa8fac2c4cc2
TJordahl90 <143350615+TJordahl90@users.noreply.github.com>
2024-12-11 00:53:24 -0600

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -160,7 +160,8 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.AllowAny',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
