30381204783a4e48bce4e8406adb41692ac471d0
TJordahl90 <143350615+TJordahl90@users.noreply.github.com>
2024-12-11 02:43:15 -0600

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -164,7 +164,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '3/hour',
