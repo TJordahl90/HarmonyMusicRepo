e553043ed886ab740d8b879456ef1e15fc9f2c66
TJordahl90 <143350615+TJordahl90@users.noreply.github.com>
2024-12-10 22:37:12 -0600

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import django_heroku
 import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
